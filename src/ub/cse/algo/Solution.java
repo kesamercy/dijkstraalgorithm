9a15099d2e6e8568bb24383506e3a98e71f89cbf
Mercy Nekesa <kesamercy@gmail.com>
2019-10-31 20:45:36 -0400

    so much more buggy code. Dijkstra's doesn't like me

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -1,10 +1,7 @@
 package ub.cse.algo;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.Objects;
+import java.util.*;
 
 
 public class Solution {
@@ -71,71 +68,100 @@ public class Solution {
          * Find the smallest weighted path between _startNode and _endNode
          * The first number of graph's adjacency list is the weight of it's node
          */
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        ArrayList<Integer> output = new ArrayList<>();
+
+        ArrayList<Integer> outPutNodes = new ArrayList<>();
         HashMap<Integer, ArrayList> tempGraph = new HashMap<>();
-        ArrayList<Integer> exploredNodes = new ArrayList<>();
-
-        tempGraph.putAll(graph);
-
-        int currentNode = _startNode;
-        int leastAmntNeibas = 1;
+        int graphSize = graph.size();
+        int[] weightsFromTheStartNode = new int[graphSize];
+        boolean[] nodesInShortestPath = new boolean[graphSize];
+        PriorityQueue<Node> pq = new PriorityQueue<>(graphSize);
         int weightPosition = 0;
-        int weightOfCurrentNode = 0;
+        int currntNode = _startNode;
+        PriorityQueue<Node> shortestPathNodes = new PriorityQueue<>(graphSize);
 
-        //end if the graph is emepty or if we find the end node
-        while (!graph.isEmpty() && currentNode != _endNode) {
-            //check if the graph is connected, that is, had at least one neighbour
+//        add the start node at the begining
+        for (int i = 0; i < weightsFromTheStartNode.length; i++) {
+            weightsFromTheStartNode[i] = Integer.MAX_VALUE;
+        }
 
-            //compute the weight for the current node
-            if (exploredNodes.indexOf(currentNode) != -1){
-                weightOfCurrentNode = graph.get(currentNode).get(weightPosition);
-            }
+        //assign the weight of the start node to the array that keeps track of the weights of each node
+        weightsFromTheStartNode[_startNode] = graph.get(_startNode).get(weightPosition);
+
+        //get the weight of the start node and add it to the pq
+        int weight = graph.get(currntNode).get(weightPosition);
+        pq.add(new Node(weight, currntNode));
+
+        while (!pq.isEmpty()) {
+
+//            remove the smallest node from the pq
+            Node removedNodePair = pq.poll();
+            int newNode = removedNodePair.getNode();
 
 
-            for (int i = 1; i < graph.get(currentNode).size(); i++) {
-                //if it has already been visited, do not add it to the nodes in the pq
-                if (exploredNodes.indexOf(i) == -1){
-                    int neighbour = graph.get(currentNode).get(i);
-                    if (exploredNodes.indexOf(neighbour) == -1 && exploredNodes.indexOf(currentNode) != -1) {
-                        int newNodeWeight = weightOfCurrentNode + graph.get(neighbour).get(weightPosition);
-                        pq.add(new Node(newNodeWeight, neighbour));
+            if (newNode == _endNode) {
+                //add the start node to the out put nodes
+                outPutNodes.add(_startNode);
+                System.out.println("I found the end node, time to get out of here");
+                //print the index for the nodes in the shortest path boolen
+                for (int i = 0; i < nodesInShortestPath.length ; i++) {
+                    if (nodesInShortestPath[i] == false){
+                        System.out.println("I came before the sucker over there " + i);
+                        outPutNodes.add(i);
+                        if (i == _endNode) {
+                            return outPutNodes;
+                        }
                     }
 
                 }
 
-            }
-            exploredNodes.add(currentNode);
-            tempGraph.get(currentNode).remove(weightPosition);
-
-            if (!output.isEmpty()) {
-                int lastElement = output.get(output.size() - 1);
-                if (tempGraph.get(lastElement).indexOf(currentNode) == -1) {
-                    output.remove(output.size() - 1);
+//                print out the array for the nodes at this point
+                System.out.println(Arrays.toString(weightsFromTheStartNode));
+                //add the start node to the output array
+                outPutNodes.add(_startNode);
+                System.out.println(outPutNodes);
+                //find the nodes with the smallest weight, if they are the end node
+                //copy the array to a pq. the pq will have the index being i and weignth being value/ sort them using compar4
+                for (int i = 0; i < weightsFromTheStartNode.length; i++) {
+                    shortestPathNodes.add(new Node(weightsFromTheStartNode[i], i));
                 }
-            }//end if output is empty
-            output.add(currentNode);
-            graph.remove(currentNode);
-            if (!pq.isEmpty()){
-                currentNode = pq.poll().node;
+                int noderetrived = 0;
+                while (noderetrived != _endNode){
+                    noderetrived = shortestPathNodes.poll().node;
+                    outPutNodes.add(noderetrived);
+                }
+                System.out.println("Node with the least dist " + outPutNodes);
+
+                System.exit(-1);
             }
 
+            //if the node doesn't exist in the nodes in the shortest path, add it
+            if (nodesInShortestPath[newNode] == false) {
+                nodesInShortestPath[newNode] = true;
 
-            while (exploredNodes.indexOf(currentNode) != -1 && !pq.isEmpty()) {
-                pq.remove();
-                currentNode = pq.poll().node;
+                //go through all the neighbours of the new node
+                for (int i = 1; i < graph.get(newNode).size(); i++) {
+                    int neighbour = graph.get(newNode).get(i);
+                    int nodeWeight = graph.get(newNode).get(weightPosition);
+
+                    //if the neighbour for the node is not in the nodes in shortest path already, meaning it's not explored, then add it
+                    if (nodesInShortestPath[neighbour] == false) {
+                        int newneighbourWeight = nodeWeight + graph.get(neighbour).get(weightPosition);
+                        int oldNeighbourWeight = weightsFromTheStartNode[neighbour];
+
+                       //check if the current weight of the node is less than the weight in the weightsfromthestrat node array
+                        if (oldNeighbourWeight > newneighbourWeight) {
+                            pq.add(new Node(newneighbourWeight, neighbour));
+                            weightsFromTheStartNode[neighbour] = newneighbourWeight;
+                        }//end if
+                    }
+
+                }
             }
+        }
 
-            if (tempGraph.get(currentNode).size() < leastAmntNeibas) {
-                output.clear();
-                return output;
-            }
-
-        }//end while
-
-
-        output.add(_endNode);
-        return output;
+        //use a prioirty to queue to add the numbers
+        //take care of the cost of the start node
+        return outPutNodes;
     }
 
 
